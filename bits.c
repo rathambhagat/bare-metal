8a411bb47ea2fd4d2039ac6683e72946342562b4
rathambhagat <prathambhagat720@gmail.com>
2026-06-01 07:37:21 +0000

    Illustrated overflow

--- bits.c
+++ bits.c
@@ -1,3 +1,19 @@
 /*Modern computers store and process information represented as two-valued
 signals. These lowly binary digits, or bits, form the basis of the digital revolution.*/
 
+
+/*Computer representations use a limited number of bits to encode a number,
+and hence some operations can overflow when the results are too large to be rep
+resented.*/ 
+#include<stdio.h>
+int main(){
+  printf("%d",200*300*400*500);
+  return 0;
+}
+// When we complie the Above function we get a warning
+
+/* bits.c: In function ‘main’:
+bits.c:8:26: warning: integer overflow in expression of type ‘int’ results in ‘-884901888’ [-Woverflow]
+    8 |   printf("%d",200*300*400*500);*/
+
+// The Output of it is a negative number = -884901888
